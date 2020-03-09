8702ddde434660c420f45ecb5fb461eee1dde42c
Benjamin Ko <benjamintko@gmail.com>
2020-03-09 18:55:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,13 @@
 # code your #position_taken? method here!
 
-# HOW COME THIS DOESN'T WORK?????
-#def position_taken?(board, index)
-#  if board[index] == " " || board[index] == "" || board[index == nil
-#    false
-#  else board[index] == "X" || "O"
-#    true
-#  end
-#end
+ HOW COME THIS DOESN'T WORK?????
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index == nil
+    false
+  else board[index] == "X" || "O"
+    true
+  end
+end
 
 # def position_taken?(board, index)
 #  if board[index] == " "
